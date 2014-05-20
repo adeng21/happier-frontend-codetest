6de8f07089487b19b4cd1f3b5fdd98f2338314d6
adeng21 <adeng21@gmail.com>
2014-05-20 17:28:26 -0400

    got videos to work
    '

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,6 +3,15 @@
 /* Controllers */
 
 angular.module('happierTest.controllers', [])
+
+  .config(function($sceDelegateProvider){
+    $sceDelegateProvider.resourceUrlWhitelist([
+      'self',
+      'https://d3mix3rw6pguam.cloudfront.net/courses/everyday-grateful/day_assets/day0-sd.mp4',
+      'https://d3mix3rw6pguam.cloudfront.net/courses/yoga-to-go/day_assets/day0-sd.mp4',
+      'https://d3mix3rw6pguam.cloudfront.net/courses/meditation-vacation/day_assets/day0-sd.mp4'
+      ]);
+  })
   .controller('videos', ['$scope', '$http', function($scope, $http) {
 
     $http.get('json/videos.json').
